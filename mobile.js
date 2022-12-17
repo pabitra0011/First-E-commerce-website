b7bc3156a2be5f9af48ba29da571c25120cb35f7
Pabitra Pal <palr74502@gmail.com>
2022-12-17 22:36:58 +0530

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,14 @@
+
+
+
+// @media screen and (min-width: 480px) {
+ 
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+// }
